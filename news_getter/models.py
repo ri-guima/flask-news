876f37501745e1ccdd07202eab16de7b86a8d7a5
ri-guima <richard.alexsander.guima@gmail.com>
2022-10-23 12:42:52 -0300

    feat: add news selector model

--- news_getter/models.py
+++ news_getter/models.py
@@ -6,9 +6,17 @@ from datetime import datetime
 Base = declarative_base()
 
 
-class News(Base):
+class NewsModel(Base):
     __tablename__ = 'news'
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     url = Column(String, nullable=False)
     created_at = Column(Datetime, default=datetime.now(), nullable=False)
+
+
+class NewsSelectorModel(Base):
+    __tablename__ = 'news_selectors'
+    id = Column(Integer, primary_key=True)
+    site_domain = Column(String, nullable=False)
+    title_selector = Column(String, nullable=False)
+    url_selector = Column(String, nullable=False)
