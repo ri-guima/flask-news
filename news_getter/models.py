debf77f9f175b0e9baf4110e94adeaa1e62b6441
ri-guima <richard.alexsander.guima@gmail.com>
2022-10-23 12:19:48 -0300

    feat: add news model

--- news_getter/models.py
+++ news_getter/models.py
@@ -1,4 +1,14 @@
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import declarative_base
+from datetime import datetime
 
 
 Base = declarative_base()
+
+
+class News(Base):
+    __tablename__ = 'news'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    created_at = Column(Datetime, default=datetime.now(), nullable=False)
