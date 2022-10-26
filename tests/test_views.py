9e455b73135427b180097ba855a139df3f3d1b32
ri-guima <richard.alexsander.guima@gmail.com>
2022-10-26 16:44:19 -0300

    tests: add tests for news page page

--- tests/test_views.py
+++ tests/test_views.py
@@ -15,3 +15,13 @@ def test_add_news_site_get(client: FlaskClient) -> None:
 def test_add_news_site_post(client: FlaskClient) -> None:
     response = client.post(url_for('flask_news.add_news_site_post'))
     assert response.status_code == 302
+
+
+def test_add_news_page_get(client: FlaskClient) -> None:
+    response = client.get(url_for('flask_news.add_news_page_get'))
+    assert response.status_code == 200
+
+
+def test_add_news_page_post(client: FlaskClient) -> None:
+    response = client.post(url_for('flask_news.add_news_page_post'))
+    assert response.status_code == 302
